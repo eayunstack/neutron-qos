eayunstack/neutron-qos

commit f317640e7759cf79a60f68f8896ca756a966b27f

qos_agent: stateless agent
Fixes: redmine #7659

Signed-off-by: huntxu <mhuntxu@gmail.com>

neutron_qos/db/qos/qos_db.py
@@ -14,21 +14,26 @@
 #    under the License.
 
 import sqlalchemy as sa
-from sqlalchemy import orm
+from sqlalchemy import orm, and_, or_
 from sqlalchemy.orm import exc
 
+from neutron.common import constants as n_constants
 from neutron.db import model_base
 from neutron.db import models_v2
 from neutron.db import common_db_mixin as base_db
+from neutron.db import l3_agentschedulers_db as l3_agent_db
 from neutron.db import l3_db
+from neutron.extensions import agent as ext_agent
 from neutron.extensions import qos as ext_qos
 from neutron.openstack.common import uuidutils
 from neutron.openstack.common import log as logging
 
 from neutron import manager
 from neutron.plugins.common import constants
+from neutron.plugins.ml2 import models as ml2_models
 
 LOG = logging.getLogger(__name__)
+NIC_NAME_LEN = 14
 
 
 class Qos(model_base.BASEV2, models_v2.HasId, models_v2.HasTenant):
@@ -72,6 +77,7 @@ class QosQueue(model_base.BASEV2, models_v2.HasId, models_v2.HasTenant):
     ceil = sa.Column(sa.BigInteger, nullable=False)
     burst = sa.Column(sa.BigInteger)
     cburst = sa.Column(sa.BigInteger)
+    tc_class = sa.Column(sa.Integer)
     qos = orm.relationship(
         Qos,
         backref=orm.backref(
@@ -110,7 +116,17 @@ class QosFilter(model_base.BASEV2, models_v2.HasId, models_v2.HasTenant):
         backref=orm.backref('attached_filters', lazy='joined', uselist=True))
 
 
-class QosDbMixin(ext_qos.QosPluginBase, base_db.CommonDbMixin):
+class QosTcClassRange(model_base.BASEV2):
+    __tablename__ = 'eayun_qostcclassranges'
+    qos_id = sa.Column(sa.String(36),
+                       sa.ForeignKey('eayun_qoss.id', ondelete='CASCADE'),
+                       nullable=False, primary_key=True)
+    first = sa.Column(sa.Integer, nullable=False)
+    last = sa.Column(sa.Integer, nullable=False)
+    qos = orm.relationship(Qos)
+
+
+class QosDb(ext_qos.QosPluginBase, base_db.CommonDbMixin):
     """ Mixin class to add security group to db_base_plugin_v2. """
 
     __native_bulk_support = True
@@ -388,6 +404,59 @@ class QosDbMixin(ext_qos.QosPluginBase, base_db.CommonDbMixin):
                 qos_queue_id=qos_queue.qos_id
             )
 
+    @staticmethod
+    def allocate_tc_class_for_queue(context, qos_queue):
+        if qos_queue.tc_class:
+            return
+        tc_class = None
+
+        with context.session.begin(subtransactions=True):
+            try:
+                if qos_queue.id == qos_queue.qos.default_queue_id:
+                    tc_class = 65534
+                else:
+                    tc_class = QosDb._try_allocate_new_tc_class(
+                        context, qos_queue.qos)
+            except ext_qos.AllocateTCClassFailure:
+                QosDb._rebuild_tc_class_range(context, qos_queue.qos)
+
+            if not tc_class:
+                tc_class = QosDb._try_allocate_new_tc_class(
+                    context, qos_queue.qos)
+
+            qos_queue.update({'tc_class': tc_class})
+
+    @staticmethod
+    def _try_allocate_new_tc_class(context, qos):
+        select_range = context.session.query(
+            QosTcClassRange).join(Qos).with_lockmode('update').first()
+        if select_range:
+            new_tc_class = select_range['first']
+            if select_range['first'] == select_range['last']:
+                LOG.debug("No more free tc class id in this slice, deleting "
+                          "range.")
+                context.session.delete(select_range)
+            else:
+                # Increse the first class id in this range
+                select_range['first'] = new_tc_class + 1
+            return new_tc_class
+        raise ext_qos.AllocateTCClassFailure(qos_id=qos.id)
+
+    @staticmethod
+    def _rebuild_tc_class_range(context, qos):
+        LOG.debug("Rebuilding tc class range for qos: %s", qos.id)
+        used_classes = sorted(
+            [1, 65534] +
+            [queue.tc_class for queue in qos.queues
+             if queue.tc_class is not None])
+        for index in range(len(used_classes) - 1):
+            if used_classes[index] + 1 < used_classes[index + 1]:
+                tc_class_range = QosTcClassRange(
+                    qos_id=qos.id,
+                    first=used_classes[index] + 1,
+                    last=used_classes[index+1] - 1)
+                context.session.add(tc_class_range)
+
     def _set_ceil_for_queues(self, max_ceil, queues):
         # Called within context.session
         for queue in queues:
@@ -586,3 +655,161 @@ class QosDbMixin(ext_qos.QosPluginBase, base_db.CommonDbMixin):
     def get_qos_filter(self, context, id, fields=None):
         qos_filter = self._get_qos_filter(context, id)
         return self._make_qos_filter_dict(qos_filter, fields)
+
+
+class QosPluginRpcDbMixin(object):
+
+    def _get_devices_for_qos(self, qos):
+        if qos.router:
+            if qos.direction == 'egress':
+                prefix = 'qg-'
+                ports = [qos.router.gw_port_id]
+            else:
+                prefix = 'qr-'
+                ports = [
+                    p.port_id
+                    for p in qos.router.attached_ports
+                    if p.port_id != qos.router.gw_port_id
+                ]
+        elif qos.port:
+            ports = [qos.port_id]
+            prefix = 'qvb' if qos.direction == 'egress' else 'qvo'
+        return [("%s%s" % (prefix, port))[:NIC_NAME_LEN] for port in ports]
+
+    def _make_qos_filter_dict_for_agent(self, qos_filter):
+        qos_filter_dict = {'prio': qos_filter.prio,
+                           'src_addr': qos_filter.src_addr,
+                           'dst_addr': qos_filter.dst_addr}
+        if qos_filter.protocol:
+            qos_filter_dict['protocol'] = qos_filter.protocol
+        if qos_filter.src_port:
+            qos_filter_dict['src_port'] = qos_filter.src_port
+        if qos_filter.dst_port:
+            qos_filter_dict['dst_port'] = qos_filter.dst_port
+        return qos_filter_dict
+
+    def _make_qos_queue_dict_for_agent(self, qos_queue):
+        qos_queue_dict = {'rate': qos_queue.rate,
+                          'ceil': qos_queue.ceil,
+                          'prio': qos_queue.prio}
+        if qos_queue.subqueues:
+            qos_queue_dict['subclasses'] = [
+                subqueue.tc_class for subqueue in qos_queue.subqueues]
+        elif qos_queue.attached_filters:
+            qos_queue_dict['filters'] = [
+                self._make_qos_filter_dict_for_agent(qos_filter)
+                for qos_filter in qos_queue.attached_filters
+            ]
+        if qos_queue.parent_queue:
+            qos_queue_dict['parent'] = qos_queue.parent_queue.tc_class
+        else:
+            qos_queue_dict['parent'] = 1
+        if qos_queue.burst:
+            qos_queue_dict['burst'] = qos_queue.burst
+        if qos_queue.cburst:
+            qos_queue_dict['cburst'] = qos_queue.cburst
+        return qos_queue_dict
+
+    def _get_qos_conf_scheme(self, context, qos):
+        root_class = {'rate': qos.rate, 'ceil': qos.rate, 'subclasses': [],
+                      'prio': 0}
+        if qos.burst:
+            root_class['burst'] = qos.burst
+        if qos.cburst:
+            root_class['cburst'] = qos.cburst
+
+        scheme = {}
+        effective_queues = {}
+        for queue in qos.queues:
+            if queue.attached_filters or queue.id == qos.default_queue_id:
+                _queue = queue
+                while _queue is not None:
+                    try:
+                        self.allocate_tc_class_for_queue(context, _queue)
+                    except ext_qos.AllocateTCClassFailure:
+                        LOG.warn("Failed to allocate tc class for queue %s.",
+                                 _queue.id)
+                        # Don't apply any qos scheme for this qos because the
+                        # number of queues exceeds Linux's support(65535).
+                        # However, this should RARELY happen.
+                        return None
+                    if _queue.id not in effective_queues:
+                        effective_queues[_queue.id] = _queue
+                        _queue = _queue.parent_queue
+                    else:
+                        # Current queue and its ancestors are all recorded.
+                        _queue = None
+
+        for queue in effective_queues.values():
+            scheme[queue.tc_class] = self._make_qos_queue_dict_for_agent(queue)
+            if queue.parent_queue is None:
+                root_class['subclasses'].append(queue.tc_class)
+
+        # Add root class
+        scheme[1] = root_class
+        return scheme
+
+    def _get_qos_for_agent(self, context, qos):
+        scheme = self._get_qos_conf_scheme(context, qos)
+        if scheme is None:
+            return None
+        return {'devices': self._get_devices_for_qos(qos),
+                'scheme': scheme}
+
+    def sync_qos(self, context, host):
+        try:
+            l3_agent = self._core_plugin._get_agent_by_type_and_host(
+                context, n_constants.AGENT_TYPE_L3, host)
+            if not l3_agent.is_active:
+                # L3 agent is dead, return nothing for routers
+                raise ext_agent.AgentNotFoundByTypeHost(
+                    agent_type=n_constants.AGENT_TYPE_L3,
+                    host=host)
+            binding_query = context.session.query(
+                l3_agent_db.RouterL3AgentBinding.router_id
+            ).filter(
+                l3_agent_db.RouterL3AgentBinding.l3_agent_id == l3_agent.id)
+            routers_bound_to_host = set(
+                binding.router_id for binding in binding_query)
+            router_query = context.session.query(
+                l3_db.Router
+            ).filter(
+                l3_db.Router.router_id.in_(routers_bound_to_host)
+            ).filter(
+                l3_db.Router.admin_state_up.is_(True))
+            routers_on_host = set(router.id for router in router_query)
+        except ext_agent.AgentNotFoundByTypeHost:
+            routers_on_host = set()
+
+        port_query = context.session.query(ml2_models.PortBinding)
+        port_query = port_query.filter(ml2_models.PortBinding.host == host)
+        ports_on_host = set(binding.port_id for binding in port_query)
+
+        query = context.session.query(Qos)
+        query = query.filter(
+            or_(
+                and_(Qos.router_id.isnot(None),
+                     Qos.router_id.in_(routers_on_host)),
+                and_(Qos.port_id.isnot(None),
+                     Qos.port_id.in_(ports_on_host))
+            )
+        )
+
+        qoss_on_host = {}
+        for qos in query.all():
+            namespace = None
+            if qos.router_id:
+                namespace = 'qrouter-' + qos.router_id
+                if namespace not in qoss_on_host:
+                    qoss_on_host[namespace] = []
+            elif qos.port_id:
+                if '_root' not in qoss_on_host:
+                    qoss_on_host['_root'] = []
+                namespace = '_root'
+
+            if namespace:
+                qos_for_agent = self._get_qos_for_agent(context, qos)
+                if qos_for_agent:
+                    qoss_on_host[namespace].append(qos_for_agent)
+
+        return qoss_on_host

bin/neutron-get-htb-conf
@@ -1,0 +1,52 @@
+#!/usr/bin/env python
+# Copyright (c) 2016 Eayun, Inc.
+# All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+This file is added ONLY because the user 'neutron' running 'neutron-qos-agent'
+could not change network namespace on the fly, ONLY root can do that.
+"""
+
+import argparse
+import json
+from neutron.services.qos.common import htb
+
+
+def get_filters(prefix):
+    def filter_by_dev_name(devname):
+        return reduce(
+            lambda x, y: devname.startswith(x) or devname.startswith(y),
+            prefix.split(','))
+
+    if prefix:
+        return filter_by_dev_name
+    else:
+        return None
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--prefix', dest='prefix', default=None,
+        help='Device prefix to filter the result, seperated by commas.'
+    )
+    args = parser.parse_args()
+    qos_conf_scheme = htb.get_qos_conf_scheme(
+        filter_by_name=get_filters(args.prefix)
+    )
+    print json.dumps(qos_conf_scheme)
+
+
+if __name__ == '__main__':
+    main()

etc/neutron/rootwrap.d/qos.filters
@@ -10,3 +10,4 @@
 
 # qos-agent
 tc: CommandFilter, tc, root
+neutron-get-htb-conf: CommandFilter, neutron-get-htb-conf, root

neutron_qos/api/rpc/agentnotifiers/qos_rpc_agent_api.py
@@ -1,92 +1,0 @@
-# Copyright (c) 2015 Eayun, Inc.
-# All rights reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-
-from oslo import messaging
-
-from neutron.common import rpc as n_rpc
-from neutron.openstack.common import log as logging
-
-LOG = logging.getLogger(__name__)
-
-QOS_AGENT = 'qos_agent'
-QOS_PLUGIN = 'qos_plugin'
-
-
-class QosAgentNotifyAPI(object):
-    """ API for plugin to notify qos agent. """
-
-    def __init__(self, topic=QOS_AGENT):
-        self.topic = topic
-        target = messaging.Target(topic=topic, version='1.0')
-        self.client = n_rpc.get_client(target)
-
-    def _notification(self, context, method, payload, host):
-        if host:
-            LOG.debug('Notify qos agent at %(topic)s.%(host)s '
-                      'the message %(method)s',
-                      {'topic': self.topic, 'host': host, 'method': method})
-            cctxt = self.client.prepare(server=host)
-            cctxt.cast(context, method, payload=payload)
-        else:
-            LOG.debug('Not notify qos agent at %(topic)s '
-                      'the message %(method)s '
-                      'due to no target host',
-                      {'topic': self.topic, 'method': method})
-
-    def qos_created(self, context, qos, host):
-        self._notification(context, 'qos_created', qos, host)
-
-    def qos_updated(self, context, qos, host):
-        self._notification(context, 'qos_updated', qos, host)
-
-    def qos_moved(self, context, qos, host):
-        self._notification(context, 'qos_moved', qos, host)
-
-    def qos_deleted(self, context, id, host):
-        self._notification(context, 'qos_deleted', id, host)
-
-    def qos_queue_created(self, context, qos_queue, host):
-        self._notification(context, 'qos_queue_created', qos_queue, host)
-
-    def qos_queue_updated(self, context, qos_queue, host):
-        self._notification(context, 'qos_queue_updated', qos_queue, host)
-
-    def qos_queue_deleted(self, context, id, host):
-        self._notification(context, 'qos_queue_deleted', id, host)
-
-    def qos_filter_created(self, context, qos_filter, host):
-        self._notification(context, 'qos_filter_created', qos_filter, host)
-
-    def qos_filter_updated(self, context, qos_filter, host):
-        self._notification(context, 'qos_filter_updated', qos_filter, host)
-
-    def qos_filter_deleted(self, context, id, host):
-        self._notification(context, 'qos_filter_deleted', id, host)
-
-
-class QosPluginRpc(object):
-
-    def __init__(self, host):
-        super(QosPluginRpc, self).__init__()
-        target = messaging.Target(topic=QOS_PLUGIN, version='1.0')
-        self.client = n_rpc.get_client(target)
-
-    def _sync_qos(self, context, qos_list):
-        try:
-            cctxt = self.client.prepare()
-            return cctxt.call(
-                context, 'sync_qos', host=self.host, qos_list=qos_list)
-        except Exception:
-            LOG.exception('Failed to sync qos.')

neutron_qos/db/migration/alembic_migrations/versions/5108f17d7012_qos_queue_tc_class.py
@@ -1,0 +1,56 @@
+# Copyright 2016 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""qos_queue_tc_class
+
+Revision ID: 5108f17d7012
+Revises: 2a853fe8a8b1
+Create Date: 2016-07-20 15:23:09.216804
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '5108f17d7012'
+down_revision = '2a853fe8a8b1'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+def upgrade():
+    op.create_table(
+        'eayun_qostcclassranges',
+        sa.Column('qos_id', sa.String(length=36), nullable=False),
+        sa.Column('first', sa.Integer(), nullable=False),
+        sa.Column('last', sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint('qos_id')
+    )
+    op.create_foreign_key(
+        'fk-eayun_qostcclassranges-qos_id-eayun_qoss',
+        'eayun_qostcclassranges', 'eayun_qoss',
+        ['qos_id'], ['id'],
+        ondelete='CASCADE'
+    )
+    op.add_column(
+        'eayun_qosqueues', sa.Column('tc_class', sa.Integer(), nullable=True)
+    )
+    qos_queues = sa.sql.table('eayun_qosqueues', sa.sql.column('tc_class'))
+    op.execute(qos_queues.update().values(tc_class=None))
+
+
+def downgrade():
+    op.drop_table('eayun_qostcclassranges')
+    op.drop_column('eayun_qosqueues', 'tc_class')

neutron_qos/db/qos/qos_rpc.py
@@ -1,198 +1,0 @@
-# Copyright (c) 2015 Eayun, Inc.
-# All rights reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-
-from oslo import messaging
-
-from neutron.db.qos import qos_db
-from neutron import manager
-from neutron.openstack.common import log as logging
-from neutron.plugins.common import constants as service_constants
-from neutron.plugins.ml2 import db  # hacky
-
-LOG = logging.getLogger(__name__)
-
-NIC_NAME_LEN = 14
-
-
-class QosServerRpcServerMixin(qos_db.QosDbMixin):
-
-    def _get_host_from_qos_id(self, context, qos_id):
-        qos = self._get_qos(context, qos_id)
-        if qos['router_id'] is not None:
-            plugin = manager.NeutronManager.get_service_plugins().get(
-                service_constants.L3_ROUTER_NAT)
-            adminContext = context if context.is_admin else context.elevated()
-            l3_agents = plugin.get_l3_agents_hosting_routers(
-                adminContext, [qos['router_id']],
-                admin_state_up=True, active=True)
-            return l3_agents[0].host
-        elif qos['port_id'] is not None:
-            """
-            plugin = manager.NeutronManager.get_plugin()
-            adminContext = context if context.is_admin else context.elevated()
-            return plugin.get_port_binding_host(adminContext, qos['port_id'])
-            """
-            return db.get_port_binding_host(qos['port_id'])
-        else:
-            return None
-
-    def _get_qos_on_host(self, context, host):
-        hosting_qos = dict()
-        for qos in self.get_qoss(context):
-            if host == self._get_host_from_qos_id(context, qos['id']):
-                qos.pop('unattached_filters')
-                qos.update(
-                    {'devices': self._get_qos_devices(context, qos['id'])})
-                hosting_qos[qos['id']] = qos
-
-        return hosting_qos
-
-    def _get_qos_devices(self, context, qos_id):
-        ret = []
-        qos_db = self._get_qos(context, qos_id)
-        if qos_db.router_id is not None:
-            plugin = manager.NeutronManager.get_service_plugins().get(
-                service_constants.L3_ROUTER_NAT)
-            adminContext = context if context.is_admin else context.elevated()
-            # A bit hacky here
-            router = plugin._get_router(adminContext, qos_db.router_id)
-            if qos_db.direction == 'egress':
-                if router.gw_port_id:
-                    ret.append(("qg-%s" % router.gw_port_id)[:NIC_NAME_LEN])
-            else:
-                for rp in router.attached_ports:
-                    if rp.port_id != router.gw_port_id:
-                        ret.append(("qr-%s" % rp.port_id)[:NIC_NAME_LEN])
-        elif qos_db.port_id is not None:
-            prefix = 'qvo' if qos_db.direction == 'ingress' else 'qvb'
-            ret.append(("%s%s" % (prefix, qos_db.port_id))[:NIC_NAME_LEN])
-
-        return ret
-
-    def sync_qos(self, context, qos_list, host):
-        hosting_qos = self._get_qos_on_host(context, host)
-        deleted_qos = filter(lambda qos: qos not in hosting_qos, qos_list)
-
-        for qos in qos_list:
-            if qos in hosting_qos:
-                hosting_qos.pop(qos)
-
-        return {'deleted': deleted_qos, 'added': hosting_qos.values()}
-
-    def create_qos(self, context, qos):
-        qos = super(
-            QosServerRpcServerMixin, self
-        ).create_qos(context, qos)
-        host = self._get_host_from_qos_id(context, qos['id'])
-        unattached_filters = qos.pop('unattached_filters')
-        qos.update({'devices': self._get_qos_devices(context, qos['id'])})
-        self.notifier.qos_created(context, qos, host)
-        qos.update({'unattached_filters': unattached_filters})
-        qos.pop('devices')
-        return qos
-
-    def update_qos(self, context, id, qos):
-        host_prev = self._get_host_from_qos_id(context, id)
-        qos = super(
-            QosServerRpcServerMixin, self
-        ).update_qos(context, id, qos)
-        host = self._get_host_from_qos_id(context, id)
-        if host_prev != host:
-            # assoicated port or router may be changed
-            self.notifier.qos_moved(context, id, host_prev)
-        unattached_filters = qos.pop('unattached_filters')
-        qos.update({'devices': self._get_qos_devices(context, id)})
-        self.notifier.qos_updated(context, qos, host)
-        qos.update({'unattached_filters': unattached_filters})
-        qos.pop('devices')
-        return qos
-
-    def delete_qos(self, context, id):
-        host = self._get_host_from_qos_id(context, id)
-        super(
-            QosServerRpcServerMixin, self
-        ).delete_qos(context, id)
-        self.notifier.qos_deleted(context, id, host)
-
-    def create_qos_queue(self, context, qos_queue):
-        qos_queue = super(
-            QosServerRpcServerMixin, self
-        ).create_qos_queue(context, qos_queue)
-        host = self._get_host_from_qos_id(context, qos_queue['qos_id'])
-        self.notifier.qos_queue_created(context, qos_queue, host)
-        return qos_queue
-
-    def update_qos_queue(self, context, id, qos_queue):
-        qos_queue = super(
-            QosServerRpcServerMixin, self
-        ).update_qos_queue(context, id, qos_queue)
-        host = self._get_host_from_qos_id(context, qos_queue['qos_id'])
-        self.notifier.qos_queue_updated(context, qos_queue, host)
-        return qos_queue
-
-    def delete_qos_queue(self, context, id):
-        qos_queue = self._get_qos_queue(context, id)
-        host = self._get_host_from_qos_id(context, qos_queue['qos_id'])
-        super(
-            QosServerRpcServerMixin, self
-        ).delete_qos_queue(context, id)
-        self.notifier.qos_queue_deleted(context, id, host)
-
-    def create_qos_filter(self, context, qos_filter):
-        qos_filter = super(
-            QosServerRpcServerMixin, self
-        ).create_qos_filter(context, qos_filter)
-        if qos_filter['queue_id']:
-            host = self._get_host_from_qos_id(context, qos_filter['qos_id'])
-            self.notifier.qos_filter_created(context, qos_filter, host)
-        return qos_filter
-
-    def update_qos_filter(self, context, id, qos_filter):
-        orig_queue_id = self.get_qos_filter(context, id)['queue_id']
-        qos_filter = super(
-            QosServerRpcServerMixin, self
-        ).update_qos_filter(context, id, qos_filter)
-        host = self._get_host_from_qos_id(context, qos_filter['qos_id'])
-        if orig_queue_id is None:
-            if qos_filter['queue_id']:
-                self.notifier.qos_filter_created(context, qos_filter, host)
-        else:
-            if qos_filter['queue_id'] is None:
-                self.notifier.qos_filter_deleted(context, id, host)
-            else:
-                self.notifier.qos_filter_updated(context, qos_filter, host)
-        return qos_filter
-
-    def delete_qos_filter(self, context, id):
-        qos_filter = self.get_qos_filter(context, id)
-        super(
-            QosServerRpcServerMixin, self
-        ).delete_qos_filter(context, id)
-        if qos_filter['queue_id']:
-            host = self._get_host_from_qos_id(context, qos_filter['qos_id'])
-            self.notifier.qos_filter_deleted(context, id, host)
-
-
-class QosRpcCallbacks(object):
-
-    target = messaging.Target(version='1.0')
-
-    def __init__(self, qos_plugin):
-        self.qos_plugin = qos_plugin
-
-    def sync_qos(self, context, **kwargs):
-        qos_list = kwargs.get('qos_list')
-        host = kwargs.get('host')
-        return self.qos_plugin.sync_qos(context, qos_list, host)

neutron_qos/extensions/qos.py
@@ -127,6 +127,10 @@ class QosTargetNotFound(nexception.Conflict):
     message = _("The following target %(target_id)s cannot be found.")
 
 
+class AllocateTCClassFailure(nexception.Conflict):
+    message = _("No more available tc class for qos %(qos_id)s.")
+
+
 def convert_to_tc_u32(value):
     """ Callers should at least catch ValueError and TypeError. """
     u32 = int(value)

commit af21036bbcd71d2185a0f42680079d204a0640bc

Fix router query error (#20)
Fixes: f317640 ("qos_agent: stateless agent")

Signed-off-by: Hunt Xu <mhuntxu@gmail.com>

neutron_qos/db/qos/qos_db.py
@@ -16,6 +16,7 @@
 import sqlalchemy as sa
 from sqlalchemy import orm, and_, or_
 from sqlalchemy.orm import exc
+from sqlalchemy.sql.expression import true
 
 from neutron.common import constants as n_constants
 from neutron.db import model_base
@@ -774,9 +775,9 @@ class QosPluginRpcDbMixin(object):
             router_query = context.session.query(
                 l3_db.Router
             ).filter(
-                l3_db.Router.router_id.in_(routers_bound_to_host)
+                l3_db.Router.id.in_(routers_bound_to_host)
             ).filter(
-                l3_db.Router.admin_state_up.is_(True))
+                l3_db.Router.admin_state_up == true())
             routers_on_host = set(router.id for router in router_query)
         except ext_agent.AgentNotFoundByTypeHost:
             routers_on_host = set()